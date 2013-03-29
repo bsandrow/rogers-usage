ce18e9211bcb300d11e411152d26b225c8855bb8
Brandon Sandrowicz <brandon@sandrowicz.org>
2013-03-29 02:14:08 -0400

    Creating rogers-current-usage script

---
+++ rogers_usage/apps/current_usage.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import getpass
+import os
+import socket
+import sys
+
+from session import RogersSession
+from usage   import current_usage_info
+
+# User Options:
+#   - Account Credentials for the My Rogers account (username/password).
+#   - "From:" address for the email.
+#   - The recipients of the email.
+#   - The SMTP host to sent the email through.
+
+from_address_default = "%s@%s" % ( getpass.getuser(), socket.gethostname() )
+
+def draft_email(info, from_addr, to_addrs):
+    """ Create an email from BW usage info. """
+
+    msg = MIMEText("""\
+Rogers Bandwidth Usage
+======================
+
+Total Usage    : %(total_usage)s GB
+    Downloads  : %(download_usage)s GB
+    Uploads    : %(upload_usage)s GB
+
+Usage Allowance: %(allowance)s GB
+Usage Left     : %(left)s GB
+
+[ For period %(billing_period)s; Usage information may be up to 48 hours old. ]
+
+""" % info)
+
+    msg['Subject'] = 'Rogers Bandwidth Usage [%s]' % (datetime.datetime.now().strftime("%Y-%B-%d %H:%M"))
+    msg['To']      = to_addrs
+    msg['From']    = from_addr
+
+    return msg
+
+def parse_args():
+    parser = argparse.ArgumentParser("""
+        Send an email summarizing Rogers bandwidth usage for the current
+        period.
+        """)
+
+    parser.add_argument('-r', '--recipients',
+                        help=""" Comma-separated list of email addresses to
+                        send the report to.  """)
+    parser.add_argument('-f', '--from', default=from_address_default,
+                        help=""" The email address that the report will be sent
+                        from. (default: %s)""" % (from_address_default) )
+    parser.add_argument('-s', '--smtp', default='localhost',
+                        help=""" The SMTP host to send the email through.
+                        (default: localhost) """)
+
+    options = parser.parse_args()
+
+    return options
+
+def read_auth():
+    config_dir = load_first_config('rogers-usage')
+    with open(os.path.join(config_dir, 'auth.json'), 'rb') as fh:
+        return json.loads(fh.read()) or {}
+
+def main():
+    options = parser_args()
+    auth    = read_auth()
+    session = RogersSession(auth['username'], auth['password'])
+    info    = current_usage_info(session)
+    msg     = draft_email(info, options.from, options.recipients)
+
+    smtplib.SMTP(options.smtp).sendmail(options.from, options.recipients, msg.as_string())
+
+def run():
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(">> Caught user interrupt. Exiting...")
